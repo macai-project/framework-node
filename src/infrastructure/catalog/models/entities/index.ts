59d238b145996b7e2e1a73a10dfd4c925acf66bd
fes300 <federicosordillo@gmail.com>
2022-03-15 10:34:37 +0100

    feat: catalog entities decoders

--- src/infrastructure/catalog/models/entities/index.ts
+++ src/infrastructure/catalog/models/entities/index.ts
@@ -1,3 +1,4 @@
+import * as D from "io-ts/Decoder";
 import { Category } from "./category";
 import { Subcategory } from "./subcategory";
 import { Microcategory } from "./microcategory";
@@ -10,32 +11,56 @@ export * from "./microcategory";
 export * from "./item";
 export * from "./tag";
 
-export interface CategoryEntity {
-  type: "category";
-  body: Category;
-}
-export interface SubcategoryEntity {
-  type: "subcategory";
-  body: Subcategory;
-}
-export interface MicrocategoryEntity {
-  type: "microcategory";
-  body: Microcategory;
-}
-export interface ItemEntity {
-  type: "item";
-  body: Item;
-}
-export interface TagEntity {
-  type: "tag";
-  body: Tag;
-}
+export const CategoryType = D.literal("category");
+export const SubcategoryType = D.literal("subcategory");
+export const MicrocategoryType = D.literal("microcategory");
+export const ItemType = D.literal("item");
+export const TagType = D.literal("tag");
 
-export type Entity =
-  | CategoryEntity
-  | SubcategoryEntity
-  | MicrocategoryEntity
-  | ItemEntity
-  | TagEntity;
+const EntityType = D.union(
+  CategoryType,
+  SubcategoryType,
+  MicrocategoryType,
+  ItemType,
+  TagType
+);
+export type EntityType = D.TypeOf<typeof EntityType>;
 
-export type EntityType = Entity["type"];
+export const CategoryEntity = D.struct({
+  type: CategoryType,
+  body: Category,
+});
+export type CategoryEntity = D.TypeOf<typeof CategoryEntity>;
+
+export const SubcategoryEntity = D.struct({
+  type: SubcategoryType,
+  body: Subcategory,
+});
+export type SubcategoryEntity = D.TypeOf<typeof SubcategoryEntity>;
+
+export const MicrocategoryEntity = D.struct({
+  type: MicrocategoryType,
+  body: Microcategory,
+});
+export type MicrocategoryEntity = D.TypeOf<typeof MicrocategoryEntity>;
+
+export const ItemEntity = D.struct({
+  type: ItemType,
+  body: Item,
+});
+export type ItemEntity = D.TypeOf<typeof ItemEntity>;
+
+export const TagEntity = D.struct({
+  type: TagType,
+  body: Tag,
+});
+export type TagEntity = D.TypeOf<typeof TagEntity>;
+
+const Entity = D.union(
+  CategoryEntity,
+  SubcategoryEntity,
+  MicrocategoryEntity,
+  ItemEntity,
+  TagEntity
+);
+export type Entity = D.TypeOf<typeof Entity>;
