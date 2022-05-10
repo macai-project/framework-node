a0d63cf4c04682c2863e37bb652ff442cf92452d
Giovanni Giordano <giovanni.giordano@macaiapp.com>
2022-05-10 15:52:34 +0200

    fix: map arguments of pino.debug

--- src/__test__/LogStore.test.ts
+++ src/__test__/LogStore.test.ts
@@ -43,7 +43,7 @@ describe.only("LogStore", () => {
     );
   });
 
-  it("when resetting the store and logs are not enabled, it prints the log", async () => {
+  it.only("when resetting the store and logs are not enabled, it prints the log", async () => {
     const logger = getPinoLogger({ name: "TEST" });
     const pinoDebugSpy = jest.spyOn(logger, "debug");
 
@@ -55,13 +55,13 @@ describe.only("LogStore", () => {
 
     expect(pinoDebugSpy.mock.calls).toEqual([
       [
+        "foo baz bar",
         {
           baz: ["bar"],
           foo: 1,
         },
-        "foo baz bar",
       ],
-      [[], "foobazbar"],
+      ["foobazbar", undefined],
     ]);
   });
 
@@ -90,5 +90,15 @@ describe.only("LogStore", () => {
     logStore.reset();
 
     expect(logStore.getCapacity()).toBe("0/3");
+  });
+
+  it("pass message and merging object to logger.debug when logs are enabled", async () => {
+    const logger = getPinoLogger({ name: "TEST" });
+    const pinoDebugSpy = jest.spyOn(logger, "debug");
+    const logStore = new LogStore(logger, Infinity, () => true);
+
+    logStore.appendLog(["LogMessageName", { foo: "bar" }]);
+
+    expect(pinoDebugSpy).toHaveBeenCalledWith("LogMessageName", { foo: "bar" });
   });
 });
